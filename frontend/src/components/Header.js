82491ad1b6458a62c1eb5c5adeb8a8e2129ea242
FarrukhGit <1803910@brunel.ac.uk>
2021-03-20 13:44:54 +0000

    Routing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
@@ -8,18 +9,24 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Green Market</Navbar.Brand>
+          <LinkContainer to='/'>
+          <Navbar.Brand>Green Market</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
+          <LinkContainer to='/cart'>
+              <Nav.Link>
                 <ShoppingCartIcon className="mr-1" />
                 Cart
               </Nav.Link>
+          </LinkContainer>
+          <LinkContainer to='/login'>
               <Nav.Link href="/login">
                 <AccountCircleIcon className="mr-1" />
                 Sign In
               </Nav.Link>
+          </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
